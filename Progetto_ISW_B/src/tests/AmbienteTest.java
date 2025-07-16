8916f5c499e1f0218239eb755db29ef962c5a57a
dpioli <diegopioli02@gmail.com>
2025-07-16 15:55:35 +0200

    Test class GestorePersistenza
    una volta che siamo andati a verificare il corretto funzionamento del
    salvataggio e caricamento dei dati possiamo essere sicuri che i dati che
    vengono manipolati all'intenro del programma verranno salvati
    correttamente.

---
+++ Progetto_ISW_B/src/tests/AmbienteTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import applicazione.CampoCaratteristico;
+import applicazione.CategoriaFoglia;
+import applicazione.CategoriaNonFoglia;
+import applicazione.Comprensorio;
+import applicazione.Gerarchia;
+import utenti.Configuratore;
+
+public class AmbienteTest {
+	
+	public Gerarchia creaAmbiente() {
+		ArrayList<String> zone = new ArrayList<>();
+		zone.add("zona1");
+		zone.add("zona2");
+		Comprensorio comprensorio = new Comprensorio("ZonaTest", zone);
+		
+		Configuratore configuratore = new Configuratore("Admin", "Test");
+		
+		HashMap<String, String> valoriCampo = new HashMap<>();
+		valoriCampo.put("Ramo 1", "Primo ramo della radice");
+		valoriCampo.put("Ramo 2", "Secondo ramo della radice");
+		
+		CampoCaratteristico campoCaratteristico = new CampoCaratteristico("CampoCTest", valoriCampo);
+		
+		CategoriaNonFoglia radice = new CategoriaNonFoglia("Radice", campoCaratteristico, true, 2);
+		
+		CategoriaFoglia foglia1 = new CategoriaFoglia("Foglia 1", 0);
+		CategoriaFoglia foglia2 = new CategoriaFoglia("Foglia 2", 1);
+		
+		radice.getSottoCateg().add(foglia1);
+		radice.getSottoCateg().add(foglia2);
+		
+		Gerarchia gerarchia = new Gerarchia(radice, configuratore, comprensorio);
+		return gerarchia;
+	}
+
+}
